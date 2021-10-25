6c3efddf88ddd82a97c61806d601aa25656029ed
Sidd <35938702+siddsp02@users.noreply.github.com>
2021-10-24 19:18:55 -0700

    Add files via upload

---
+++ bitcoin_calculator.py
@@ -0,0 +1,42 @@
+import sys
+
+BASE_REWARD = 50
+REDUCTION = 1 / 2
+INTERVAL = 210_000
+MAX_SUPPLY = 21_000_000
+
+
+def block_reward(height: int) -> float:
+    """Calculates the block reward for Bitcoin given the block height."""
+    return BASE_REWARD * REDUCTION ** (height // INTERVAL)
+
+
+def circulating_supply(height: int) -> float:
+    """Calculates the circulating supply of Bitcoin given the block height."""
+
+    if height < 0:
+        raise ValueError("Height must be a non-negative integer.")
+    elif not isinstance(height, int):
+        raise TypeError("Height must be an integer.")
+
+    height += 1
+    HALVING_CYCLES = height // INTERVAL
+
+    return BASE_REWARD * REDUCTION ** HALVING_CYCLES * (
+        height / INTERVAL - HALVING_CYCLES
+    ) * INTERVAL + sum(
+        MAX_SUPPLY * REDUCTION ** x for x in range(1, HALVING_CYCLES + 1)
+    )
+
+
+def main(block: int) -> None:
+    circulating = circulating_supply(block)
+    subsidy = block_reward(block)
+    print(
+        f"At block {block:d}, the number of circulating bitcoins are {circulating:.8f}",
+        f"and the block subsidy is {subsidy:.8f} Bitcoins per block",
+    )
+
+
+if __name__ == "__main__":
+    main(int(sys.argv[1]))
